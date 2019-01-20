8a12f7bffd5e57dcf93ca3d02f1f1a9b456a3632
Marcus Mueller-Westerholt <mmuellerwest@de.ibm.com>
2019-01-20 12:36:21 +0100

    Task #001: add categorie watermark
    add directory name as category watermark on image lower right corner

--- scale-for-frame.py
+++ scale-for-frame.py
@@ -3,17 +3,23 @@
 # scale-for-frame is the main module, reading .jpg files from source directories named "_xxx" and
 # writes them as scaled .jpg images to target directories named "xxx"
 #
-# M. Mueller-Westerholt, ma2018@mueller-westerholt.de
+# M. Mueller-Westerholt, magit@mueller-westerholt.de
 #
-
-from PIL import Image
-from os import listdir
+import warnings
+from os import listdir, path
 from os.path import join, isdir, isfile
 from pathlib import Path
-from shutil import copystat
 
-workdir="C:/Users/Marcus/Documents/Photoframe"
-targetsize = 1024, 600
+from PIL import Image, ImageDraw, ImageFont, ImageStat
+
+# workdir="C:/Uoers/Marcus/Documents/Photoframe"
+workDir = '/home/marcus/git/photoframescale/images'
+targetSize = 1024, 600
+fontName = 'LHANDW.TTF'
+initialFontSize = 32
+textBorder = (50, 50)
+textColorBright = (240, 240, 240)
+textColorDark = (16, 16, 16)
 
 def buildTargetDirName(topicdirname):
     "Create a target directory file name from a topic directory file name, i.e. create xxx from _xxx"
@@ -21,8 +27,47 @@ def buildTargetDirName(topicdirname):
         return topicdirname[1:]
     return "__" + topicdirname
 
-def convertTopicDir(fulltopicdir, fulltargetdir):
+def getFontAtSize(fontsPath, fontName, initialFontSize, textToPrint, targetWidth):
+    "Get the font at the right size so the text width won't exceed the targetWidth"
+    fontSize = initialFontSize
+    while True:
+        font = ImageFont.truetype(path.join(fontsPath, fontName), fontSize)
+        textWidth = font.getsize(textToPrint)[0]
+        if textWidth <= targetWidth:
+            break
+        if fontSize < 9:
+            break
+        fontSize = fontSize - 1
+    return font
+
+def getMatchingTextColor(im, pos, size):
+    "Get a bright or a dark color combination so the text will best stand out from the image"
+    partArea = (pos[0], pos[1], pos[0] + size[0], pos[1] + size[1])
+    partImage = im.crop(partArea).convert('L')
+    stat = ImageStat.Stat(partImage)
+    meanBrightness = stat.mean[0]
+    if meanBrightness < 128:
+        return textColorBright
+    else:
+        return textColorDark
+
+def addCaptionToImage(im, captionText):
+    "Add a caption to the lower right corner of the image. Font size is decremented until the text fits into half the image width"
+    draw = ImageDraw.Draw(im)
+    currentDir = path.dirname(__file__)
+    fontsPath = path.join(currentDir, 'fonts')
+    imageSize = im.size
+    imageWidth = imageSize[0]
+    captionFont = getFontAtSize(fontsPath, fontName, initialFontSize, captionText, imageWidth / 2)
+    textSize = captionFont.getsize(captionText)
+    textStartPos = imageSize[0] - textSize[0] - textBorder[0], imageSize[1] - textSize[1] - textBorder[1]
+    textColor = getMatchingTextColor(im, textStartPos, textSize)
+    draw.text(textStartPos, captiontext, textColor, font=captionFont)
+    return im
+
+def convertTopicDir(fulltopicdir, fulltargetdir, captiontext):
     "Convert a directory full of .jpg / .jpeg files to scaled size"
+    warnings.simplefilter('error', Image.DecompressionBombWarning)
     topicfiles = [f for f in listdir(fulltopicdir) if isfile(join(fulltopicdir, f)) \
                   and (f.lower().endswith(".jpg") or f.lower().endswith(".jpeg"))]
     for topicfile in topicfiles:
@@ -31,20 +76,22 @@ def convertTopicDir(fulltopicdir, fulltargetdir):
         print("Converting", topicfile)
         try:
             im = Image.open(fulltopicfile)
-            im.thumbnail(targetsize, Image.ANTIALIAS)
+            im.thumbnail(targetSize, Image.ANTIALIAS)
+            im = addCaptionToImage(im, captiontext)
             im.save(fulltargetfile, "JPEG")
         except IOError:
             print("cannot create target for '%s'" % fulltopicfile)
 
 # --- main ---
-topicdirs = [f for f in listdir(workdir) if isdir(join(workdir, f)) and f.startswith("_")]
+topicdirs = [f for f in listdir(workDir) if isdir(join(workDir, f)) and f.startswith("_")]
 
 print("Topic dirs are", topicdirs)
 
 for topicdir in topicdirs:
     targetdir = buildTargetDirName(topicdir)
+    captiontext = targetdir # change this if the caption should be different from the dir name
     print("Convert and transfer from", topicdir, "to", targetdir)
-    fulltopicdir = join(workdir, topicdir)
-    fulltargetdir = join(workdir, targetdir)
+    fulltopicdir = join(workDir, topicdir)
+    fulltargetdir = join(workDir, targetdir)
     Path(fulltargetdir).mkdir(exist_ok=True)
-    convertTopicDir(fulltopicdir, fulltargetdir)
+    convertTopicDir(fulltopicdir, fulltargetdir, captiontext)
